cf6fd35b68d6cc172d96b7be3e3ded8845c1da02
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-10-17 15:53:32 +0530

    second commit

--- Frontend/src/components/Topbar.jsx
+++ Frontend/src/components/Topbar.jsx
@@ -6,23 +6,24 @@ import Button from 'react-bootstrap/Button';
 const Topbar = () => {
   return (
    <>
-     <Navbar expand="lg" className="bg-body-tertiary">
+     <Navbar expand="lg" className="bg-body-tertiary shadow">
       <Container fluid>
         <Navbar.Brand href="/" style={{fontSize:"25px",fontWeight:"bold",padding:"10px",color:"orange"}}>Foody Hunt</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="ms-auto my-2 my-lg-0"
+            className="mx-auto my-2 my-lg-0"
             style={{ maxHeight: '100vh',maxWidth:"100vw",textAlign:"center"}}
             navbarScroll
           >
-            <Nav.Link href="/" className='p-2 home'>Home</Nav.Link>
-            <Nav.Link href='/About' className='p-2 about'>About</Nav.Link>
-            <Nav.Link href='/service' className='p-2 service'>Services</Nav.Link>
-            <Nav.Link href='/contact' className='p-2 contact'>Contact</Nav.Link>
-            <Nav.Link href='/policy' className='p-2 terms'>Terms & Policy</Nav.Link>
+            <Nav.Link href="/" className='p-2 home text-dark'>Home</Nav.Link>
+            <Nav.Link href='/About' className='p-2 about text-dark'>About</Nav.Link>
+            <Nav.Link href='/service' className='p-2 service text-dark'>Services</Nav.Link>
+            <Nav.Link href='/contact' className='p-2 contact text-dark'>Contact</Nav.Link>
+            <Nav.Link href='/policy' className='p-2 terms text-dark'>Terms & Policy</Nav.Link>
           </Nav>
-           <Nav.Link href='/login' className='p-2 login' style={{}}>Login</Nav.Link>
+           <Nav.Link href='/login' className='p-2' style={{textAlign:"center"}}>Login</Nav.Link>
+           <Nav.Link href='/cart' className='p-2' style={{textAlign:"center"}}>Cart</Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import {Container,Row,Col} from 'react-bootstrap';
+import Topbar from '../components/Topbar';
+import food from '../assets/foodimage.jpg'
 const Home = () => {
   return (
     <>
+     <Container fluid style={{}}>
+      <Row>
       <p>Home</p>
+      </Row>
+     </Container>
     </>
   )
 }
