c1bbffecb605bd55c3cb0a7f5bc421e057e61822
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-10-29 02:40:46 +0530

    code add

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <>
             <Container fluid className='bg-body-tertiary shadow'>
-                <h1 className='m-2' style={{ fontWeight: "700", fontFamily: "initial", fontSize: "50px", color: "orange" }}>Foody Hunt</h1>
+                <h1 className='m-2' style={{ fontWeight: "700", fontFamily: "initial", fontSize: "40px", color: "orange" }}>Foody Hunt</h1>
                 <Row className='m-2'>
                     <Col xs={12} md={4} lg={3}>
                         <p style={{ fontWeight: "400", fontFamily: "initial", fontSize: "25px" }}>About Foody</p>
@@ -26,7 +26,7 @@ const Footer = () => {
                             <a href='/contact' style={{ textDecoration: "none", color: "black" }}>Contact</a>
                         </li>
                         <li style={{ listStyle: "none", padding: "5px" }}>
-                            <a href='terms' style={{ textDecoration: "none", color: "black" }}>Terms&Policy</a>
+                            <a href='terms' style={{ textDecoration: "none", color: "black" }}>Terms & Policy</a>
                         </li>
                     </Col>
 
@@ -50,19 +50,23 @@ const Footer = () => {
                     {/* ============================================================================================ */}
                     <Col xs={12} md={4} lg={3}>
                         <p style={{ fontWeight: "400", fontFamily: "initial", fontSize: "25px" }}>Social Links</p>
-                            <ul className='d-flex' style={{textDecoration:"none",listStyle:"none",padding:0,margin:0}}>
-                                <li className='p-2'><a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer"><BsInstagram style={{color:"red"}}/></a></li>
-                                <li className='p-2'><a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer"><FaFacebook /></a></li>
-                                <li className='p-2'><a href='https://www.twitter.com' target="_blank" rel="noopener noreferrer"><FaXTwitter style={{color:"black"}}/></a></li>
-                                <li className='p-2'><a href='https://www.youtube.com' target="_blank" rel="noopener noreferrer"><FaYoutube style={{color:"red"}}/></a></li>
-                                <li className='p-2'><a href='https://www.linkedin.com' target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></li>
-                            </ul>
-                            <p><img className='mt-3' src='https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png'  alt='app' height={50} width={200}></img></p>
-                            <p><img className='mt-1' src='https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png'  alt='app' height={50} width={200}></img></p>
+                        <ul className='d-flex' style={{ textDecoration: "none", listStyle: "none", padding: 0, margin: 0 }}>
+                            <li className='p-2'><a href='https://www.instagram.com' target="_blank" rel="noopener noreferrer"><BsInstagram style={{ color: "red" }} /></a></li>
+                            <li className='p-2'><a href='https://www.facebook.com' target="_blank" rel="noopener noreferrer"><FaFacebook /></a></li>
+                            <li className='p-2'><a href='https://www.twitter.com' target="_blank" rel="noopener noreferrer"><FaXTwitter style={{ color: "black" }} /></a></li>
+                            <li className='p-2'><a href='https://www.youtube.com' target="_blank" rel="noopener noreferrer"><FaYoutube style={{ color: "red" }} /></a></li>
+                            <li className='p-2'><a href='https://www.linkedin.com' target="_blank" rel="noopener noreferrer"><FaLinkedin /></a></li>
+                        </ul>
+                        <p><img className='mt-3' src='https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png' alt='app' height={50} width={200}></img></p>
+                        <p><img className='mt-1' src='https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png' alt='app' height={50} width={200}></img></p>
                     </Col>
 
                     {/* ============================================================================================ */}
                 </Row>
+                <footer className="text-center mt-3">
+                    <hr style={{ margin: 0, padding: 0, borderBottom: "1px solid black" }} />
+                    <p style={{ margin: 0, padding: "5px 0" }}> © {new Date().getFullYear()} Created by Md Tauqueer Manzar . All rights reserved.</p>
+                </footer>
             </Container>
         </>
     )
