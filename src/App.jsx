a4c77a9186471b52bfd72dcfddba25121580ce2a
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-10-31 03:30:21 +0530

    code add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Topbar from './components/Topbar';
 import Service from './pages/Service';
 import Policy from './pages/Policy';
 import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import PlaceOrder from './pages/PlaceOrder';
 import './App.css'
 function App() {
 
@@ -19,6 +21,8 @@ function App() {
         <Route path='/service' element={<Service/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/polciy' element={<Policy/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
       </Routes>
     </Router>
     </>
