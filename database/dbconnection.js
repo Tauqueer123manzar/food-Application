2837b30eeb8ae412394b5642dd64413dfe4446fa
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-11-24 11:04:02 +0530

    Backend setup

---
+++ database/dbconnection.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+
+dotenv.config({path:"./config/.env"});
+main().then(()=>{
+    console.log("!!!Mongodb database connection sucessfully!!!!");
+ }).catch((error)=>{
+     console.log(`${error.message}`);
+ })
+ 
+ async function main(){
+     await mongoose.connect(process.env.MONGO_URI,{
+         useNewUrlParser:true,
+         useUnifiedTopology:true
+     });
+ };
+
+module.exports=mongoose.connection;
