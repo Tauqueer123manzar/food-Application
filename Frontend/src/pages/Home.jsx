ae083377ebfa7e6cd87d0e47698501d37b019963
Tauqueer Manzar <mdtauqueermanzar@gmail.com>
2025-02-03 01:44:40 +0530

    code added

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -15,13 +15,13 @@ const Home = () => {
     <>
       {/* Full-screen container */}
       <Container fluid style={{ width: "100%", height: "100vh", padding: 0, margin: 0 }}>
-        
+
         {/* Particles background only for the top section */}
         <div style={{
-          position: "absolute", 
+          position: "absolute",
           top: 0, left: 0,
-          right:0, 
-          width: "100%", 
+          right: 0,
+          width: "100%",
           // height: "100vh",
           zIndex: -1
         }}>
@@ -29,7 +29,7 @@ const Home = () => {
         </div>
 
         {/* Main content section */}
-        <Row className='mt-5' style={{margin:0,padding:0,left:0,right:0}}>
+        <Row className='mt-5' style={{ margin: 0, padding: 0, left: 0, right: 0 }}>
 
           {/* Left Section: Text */}
           <Col xs={12} md={6} lg={5} className="d-flex flex-column justify-content-center text-center text-md-start">
@@ -47,19 +47,15 @@ const Home = () => {
             <p className='p-4' style={{ fontSize: "22px", fontFamily: "initial", color: "white" }}>
               Explore the best flavors from local favorites and international cuisines. Get your cravings satisfied with quick and easy delivery, right to your door.
             </p>
-            <h3 className='p-4' style={{ fontFamily: "cursive" }}>Explore Our Popular Categories</h3>
-            <Button className='btn btn-outline-warning service_button' href='/service' style={{ fontWeight: "600", fontSize: "20px", height: "55px", width: "180px", marginLeft: "25px" }}>
-              Explore Services
-            </Button>
           </Col>
 
           {/* Right Section: Image */}
           <Col xs={12} md={6} lg={7} className="d-flex justify-content-center">
-            <img 
-              src={food} 
-              className="rounded mt-2" 
-              alt='food image' 
-              style={{ width: "100%", height: "auto" }} 
+            <img
+              src={food}
+              className="rounded mt-2"
+              alt='food image'
+              style={{ width: "100%", height: "auto" }}
               data-aos="zoom-in"
               data-aos-duration="2000"
               data-aos-once="true"
@@ -69,12 +65,12 @@ const Home = () => {
         </Row>
 
         {/* Food categories section */}
-        <div style={{ position: "relative", zIndex: 1, backgroundColor: "white",margin:0,padding:0,top:0,left:0}}>
+        <div style={{ position: "relative", zIndex: 1, backgroundColor: "white", margin: 0, padding: 0, top: 0, left: 0 }}>
           <Exploremenu category={category} setCategory={setCategory} />
           <Foodmenu />
-          
+
           <hr style={{ border: "1px solid gray", margin: "0", padding: "0" }} />
-          
+
           {/* Footer */}
           <Footer />
         </div>
